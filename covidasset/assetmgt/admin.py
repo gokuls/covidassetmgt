fc83e541cd6509a64d005ee6c94f1c8824137fb4
sakthisgit <sakthisaravanan.p@gmail.com>
2020-04-13 01:28:34 +0530

    Models registered

--- covidasset/assetmgt/admin.py
+++ covidasset/assetmgt/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+admin.site.register(State)
+admin.site.register(District)
+admin.site.register(Hospital)
+admin.site.register(Asset)
+admin.site.register(AssetMgt)
+admin.site.register(PatientStat)
+
--- covidasset/assetmgt/views.py
+++ covidasset/assetmgt/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import Http404
 
+
+from .models import Asset
 # Create your views here.
+
+def Asset(request):
+    """ Asset Master Details"""
+
+    try:
+        asset_val = Asset.objects.all()
+    except Asset.DoesNotExist:
+        raise Http404('No Data Found')
+    return render(request,'assetmgt/asset.html',{'asset':asset_val})
